43ac3509682705253a7321a89dfeab2e0b73894a
Débora Inocêncio <30512134+MaisQueJogos@users.noreply.github.com>
2021-10-17 20:20:28 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
- var myWords = ["EGG", "MILK", "BUTTER", "JAM", "OATS", ]
-$(document) .ready(function(){
-                   
+ var myWords = ["EGG", "MILK", "BUTTER", "JAM", "OATS", "SUGAR","BREAD","RUSK"];
+$(document).ready(function(){
+    arrangeGame();                   
 });
-    
+function arrangeGame()
+{
+  $.each(myWords, function(key, item){
+      $("#hint").append("<p>" + item + "</p>");
+  })
+}
