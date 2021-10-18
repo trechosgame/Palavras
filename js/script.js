81eca4a73761e80b58900af381bd3447a23a7dff
Débora Inocêncio <30512134+MaisQueJogos@users.noreply.github.com>
2021-10-18 23:53:22 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,4 +7,11 @@ function arrangeGame()
   $.each(myWords, function(key, item){
       $("#hint").append("<p>" + item + "</p>");
   });
+  for(var i=1;i<=12;i++)
+  {
+     for(var j=1;j<=12;j++)
+     {
+       $("#letters").append("<div class=individual data-row=" + i + " data-column=" + j + "></div>");
+     }
+  }
 }
