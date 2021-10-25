2a123f8d09c412831c3330999eaea880bf8d8520
Débora Inocêncio <30512134+MaisQueJogos@users.noreply.github.com>
2021-10-25 19:13:29 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 var myWords = ["EGG","MILK","BUTTER","JAM","OATS","SUGAR","BREAD","RUSK"];
 $(document).ready(function(){
-    arrangeGame();                   
+    arrangeGame();
 });
+function arrangeGame()
+{
+  $.each(myWords, function(key, item){
+      $("#hint").append("<p>" + item + "<"/p">);
+  });
+  for(var i=1;i<=12;i++)
+  {
+      for(var j=1;j<=12;j++)
+      {
+          $("#letters").append("div class=individual data-
+          row=" + i +  " data-column=" + j + "></div>")
+      
+      }
+  
+  }
+}
